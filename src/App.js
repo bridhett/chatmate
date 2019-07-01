f252239617363763bca37c6e51ee2fc26399a84e
Bridhett Puerto-Ulloa <bridhett17@yahoo.com>
2019-06-30 23:01:09 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo2.png';
 import TextInput from './TextInput.js'
 
 function App() {
+  
   return (
     <div className="App">
       <header className="header">
--- src/TextInput.js
+++ src/TextInput.js
@@ -2,11 +2,35 @@ import React from 'react';
 
 class TextInput extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+// I tried updating the DOM with the messages inputted by user but failed.
+// Somehow keeps pasting the input over and over in input bar.
+  handleSubmit(event) {
+    let p = this.state.value;
+    this.setState({ value: this.state.value.concat(p)});
+    event.preventDefault();
+  }
+
+
   render(){
-    return(<div className="text-input">
-      <input />
-      <button>↑</button>
-    </div>)
+    return(<form className="text-input" onSubmit={this.handleSubmit}>
+      <input placeholder="new message" type="text" value={this.state.value} onChange={this.handleChange} />
+      <input className="button" type="submit" value="↑" />
+
+    </form>)
   }
 
 }
