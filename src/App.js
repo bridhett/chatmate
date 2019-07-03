ed53b95bc5f591de0db08ae1dd8f2d7b99c5b65d
Bridhett Puerto-Ulloa <bridhett17@yahoo.com>
2019-07-03 12:42:49 -0700

    added display of messages

--- src/App.js
+++ src/App.js
@@ -2,18 +2,47 @@ import React from 'react';
 import './App.css';
 import logo from './logo2.png';
 import TextInput from './TextInput.js'
+import NamePicker from './NamePicker.js'
 
-function App() {
+class App extends React.Component {
   
-  return (
-    <div className="App">
-      <header className="header">
-        <img src={logo} alt="logo" className="logo" />
-        hatmate
-      </header>
-      <TextInput />
-    </div>
-  );
-}
+  state = {
+    messages: [],
+    name: '',
+    editName: false
+  }
 
+  gotMessage = (m) => {
+    // ... spread operator takes takes everything in messages array and adds m which is the new message
+    var newMessagesArray = [...this.state.messages, m]
+    this.setState({messages: newMessagesArray})
+    // same as this.setState({messages: [...this.state.messages, m]})
+  }
+
+  render() {
+    var {messages} = this.state
+    console.log(messages)
+    return (
+      <div className="App">
+        <header className="header">
+          <div>
+          <img src={logo} alt="logo" className="logo" />
+          hatmate
+          </div>
+          <NamePicker />
+      </header>
+      <main className="messages">
+        {messages.map((m, i)=>{
+          return (<div key={i} className="bubble-wrap">
+            <div className="bubble">
+              <span>{m}</span>
+            </div>
+          </div>)
+        })}
+      </main>
+        <TextInput sendMessage={this.gotMessage} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,36 +1,37 @@
 import React from 'react';
+import { TiArrowUpOutline } from "react-icons/ti";
 
 class TextInput extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: []
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+   
+  state = {
+    text:"",
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  // fat arrow allows you to use this inside function
+  send = () => {
+    // props live in the Apps.js file e.g. <TextInput />
+    this.props.sendMessage(this.state.text)
+    this.setState({text:""})
   }
 
-// I tried updating the DOM with the messages inputted by user but failed.
-// Somehow keeps pasting the input over and over in input bar.
-  handleSubmit(event) {
-    let p = this.state.value;
-    this.setState({ value: this.state.value.concat(p)});
-    event.preventDefault();
+  keyPress = (e) => {
+    console.log(e.key)
+    if(e.key === 'Enter') {
+      this.send()
+    }
   }
-
 
   render(){
-    return(<form className="text-input" onSubmit={this.handleSubmit}>
-      <input placeholder="new message" type="text" value={this.state.value} onChange={this.handleChange} />
-      <input className="button" type="submit" value="↑" />
+    var {text} = this.state
+    return(<div className="text-input">
+      <input placeholder="new message" type="text" value={text} 
+      onChange={e => this.setState({text: e.target.value})} 
+      onKeyPress={this.keyPress} />
+      <button disabled={!text} onClick={this.send}>
+        <TiArrowUpOutline />
+      </button>
 
-    </form>)
+    </div>)
   }
 
 }
