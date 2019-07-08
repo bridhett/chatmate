e173faa66a623d04f8cf8c91359fb8368b0f6db9
Bridhett Puerto-Ulloa <bridhett17@yahoo.com>
2019-07-07 18:44:37 -0700

    username added

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ class App extends React.Component {
   state = {
     messages: [],
     name: '',
-    editName: false
+    editName: false,
   }
 
   gotMessage = (m) => {
+    const message = {
+      text: m,
+      from: this.state.name
+    }
     // ... spread operator takes takes everything in messages array and adds m which is the new message
-    var newMessagesArray = [...this.state.messages, m]
+    var newMessagesArray = [...this.state.messages, message]
     this.setState({messages: newMessagesArray})
     // same as this.setState({messages: [...this.state.messages, m]})
   }
@@ -25,17 +29,22 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="header">
-          <div>
+          <div className="app-title">
           <img src={logo} alt="logo" className="logo" />
           hatmate
           </div>
-          <NamePicker />
+          <NamePicker 
+          name={this.state.name}
+          editName={this.state.editName}
+          changeName={name=> this.setState({name})}
+          setEditName={editName=> this.setState({editName})}/>
       </header>
       <main className="messages">
         {messages.map((m, i)=>{
           return (<div key={i} className="bubble-wrap">
             <div className="bubble">
-              <span>{m}</span>
+              <div className="bubble-name">{m.from}</div>
+              <span>{m.text}</span>
             </div>
           </div>)
         })}
